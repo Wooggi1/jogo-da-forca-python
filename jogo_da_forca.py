7d7469546222cf6b027e6f4a4b658bb184a4bfa8
Thiago Silva <86689822+Wooggi1@users.noreply.github.com>
2024-03-27 03:08:10 -0300

    Update jogo_da_forca.py

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,31 +1,32 @@
 print('*****************Forca*****************')
 
 palavra_reservada = 'Brasil'
-lista_palavra = ['_' for x in range(len(palavra_reservada))]
+palavra_reservada_lower = palavra_reservada.lower() 
+lista_palavra = ['_' if letra != ' ' else ' ' for letra in palavra_reservada]
 limite = 0
 lista_boneco = [' o\n', '/', '|', '\\ \n', '/', ' \\']
 
-while(limite < len(lista_boneco)):
+while limite < len(lista_boneco):
     for letra in lista_palavra:
         print(letra, end=' ')
 
     print('\n')
 
-    chute = input('Digite a letra: ')
+    chute = input('Digite a letra: ').lower()
 
-    if chute not in palavra_reservada:
+    if chute not in palavra_reservada_lower:
         print('Tente de novo')
         limite += 1
         print(''.join(lista_boneco[:limite]))
         continue
     
-    for i, letra in enumerate(palavra_reservada):
+    for i, letra in enumerate(palavra_reservada_lower):
         if letra == chute:
-            lista_palavra[i] = chute
+            lista_palavra[i] = palavra_reservada[i]
 
-    if palavra_reservada == ''.join(lista_palavra):
+    if palavra_reservada_lower == ''.join(letra.lower() for letra in lista_palavra):
         print('ggez')
-        print(f'resposta: {''.join(lista_palavra)}')
+        print(f'resposta: {"".join(lista_palavra)}')
         break
 
 if limite == len(lista_boneco):
